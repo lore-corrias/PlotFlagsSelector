5cd9d31a78f994a49c36eb14ff4ef242de710c47
justlel <32979892+justlel@users.noreply.github.com>
2019-09-04 00:59:06 +0200

    :bug: Fixing ASync dispatchCommand and bad Integer parsing
    :bug: Fixing ASync dispatchCommand and bad Integer parsing

--- src/main/java/com/justlel/plotflagsselector/listeners/ChatListener.java
+++ src/main/java/com/justlel/plotflagsselector/listeners/ChatListener.java
@@ -4,6 +4,8 @@ import com.justlel.plotflagsselector.Main;
 import com.justlel.plotflagsselector.commands.FlagsCommand;
 import com.justlel.plotflagsselector.iomanager.IOManager;
 import com.justlel.plotflagsselector.yaml.LanguageYaml;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -30,14 +32,18 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         if (IOManager.isPlayerWaiting(e.getPlayer())) {
-            FlagsCommand.SUB_CATEGORIES category = IOManager.getPlayerWaitingCategory(e.getPlayer());
+            String message = e.getMessage();
+            Player player = e.getPlayer();
+            FlagsCommand.SUB_CATEGORIES category = IOManager.getPlayerWaitingCategory(player);
             if (category == null)
                 return;
-            if (!validateMessage(category, e.getMessage())) {
-                e.getPlayer().sendMessage(getMain().getLanguageConfiguration().getMessage(LanguageYaml.LANGUAGES_INDEX.INVALID_FLAG_PARAMETER));
+            if (!validateMessage(category, message)) {
+                player.sendMessage(getMain().getLanguageConfiguration().getMessage(LanguageYaml.LANGUAGES_INDEX.INVALID_FLAG_PARAMETER));
             } else {
-                e.getPlayer().performCommand("plot flag set " + category + " " + e.getMessage());
-                IOManager.removePlayerWaiting(e.getPlayer());
+                if(!Bukkit.isPrimaryThread()) {
+                    Bukkit.getScheduler().runTask(getMain(), new RunCommand(category, player, message));
+                }
+                IOManager.removePlayerWaiting(player);
             }
             e.setCancelled(true);
         }
@@ -52,7 +58,7 @@ public class ChatListener implements Listener {
     private boolean validateMessage(FlagsCommand.SUB_CATEGORIES category, String message) {
         if (category == FlagsCommand.SUB_CATEGORIES.FEED || category == FlagsCommand.SUB_CATEGORIES.HEAL) {
             try {
-                Integer number = Integer.parseInt(message);
+                int number = Integer.parseInt(message);
                 if(number < 0 || number > 20)
                     return false;
             } catch (NumberFormatException e) {
@@ -60,6 +66,24 @@ public class ChatListener implements Listener {
             }
         }
         return true;
+    }
+
+    private class RunCommand implements Runnable {
+
+        private FlagsCommand.SUB_CATEGORIES category;
+        private Player player;
+        private String value;
+
+        RunCommand(FlagsCommand.SUB_CATEGORIES category, Player player, String value) {
+            this.category = category;
+            this.player = player;
+            this.value = value;
+        }
+
+        @Override
+        public void run() {
+            player.performCommand("plot set " + category.toString().toLowerCase() + " " + value);
+        }
     }
 
     /**
